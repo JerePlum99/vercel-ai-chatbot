839d37700e7e8598976705c5e22973aaa4a9abdf
JerePlum99 <102932968+JerePlum99@users.noreply.github.com>
2025-03-05 14:31:40 -0600

    Minor adjustment for added security

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'avatar.vercel.sh',  // Allow Vercel avatar service
-      'lh3.googleusercontent.com',  // Allow Google profile pictures
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/a/**',  // Google profile pics follow this pattern
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+        pathname: '/**',
+        port: '',
+      },
     ],
+    minimumCacheTTL: 60, // Cache successful image loads for 60 seconds
+    deviceSizes: [32, 48, 64, 96], // We only use small avatar sizes
+    imageSizes: [], // We don't need additional sizes for avatars
   },
 };
 
